575e1cd299550c518580a208b3d5385e791dc645
Blake Wood Jr <blakewoodjr84@gmail.com>
2023-08-03 11:56:06 -0700

    Added simpsons repository

---
+++ packages/simpsons_repository/lib/simpsons_repository.dart
@@ -0,0 +1,4 @@
+/// A Very Good Project created by Very Good CLI.
+library simpsons_repository;
+
+export 'src/simpsons_repository.dart';
---
+++ packages/simpsons_repository/lib/src/simpsons_repository.dart
@@ -0,0 +1,7 @@
+/// {@template simpsons_repository}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class SimpsonsRepository {
+  /// {@macro simpsons_repository}
+  const SimpsonsRepository();
+}
