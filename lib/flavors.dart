b07f4b2cbbba24139ba9b7a3b833a6a0e926c945
Blake Wood Jr <blakewoodjr84@gmail.com>
2023-08-03 11:34:43 -0700

    Added flavors.

---
+++ lib/flavors.dart
@@ -0,0 +1,21 @@
+enum Flavor {
+  simpsons,
+  wire,
+}
+
+class F {
+  static Flavor? appFlavor;
+
+  static String get name => appFlavor?.name ?? '';
+
+  static String get title {
+    switch (appFlavor) {
+      case Flavor.simpsons:
+        return 'Simpsons Character View';
+      case Flavor.wire:
+        return 'The Wire Character Viewer';
+      default:
+        return 'title';
+    }
+  }
+}
